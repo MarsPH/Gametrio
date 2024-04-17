0c1a1090426f55ef340fd8557e62f1c59f82caaa
AegonSnowX <156529617+AegonSnowX@users.noreply.github.com>
2024-04-17 13:27:13 -0400

    feat:Added how to play module

--- Howtoplay.Designer.cs
+++ Howtoplay.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label1 = new Label();
+            label2 = new Label();
             SuspendLayout();
             // 
             // button1
@@ -42,22 +46,71 @@
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // button2
+            // 
+            button2.Font = new Font("Showcard Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.Location = new Point(12, 65);
+            button2.Name = "button2";
+            button2.Size = new Size(236, 47);
+            button2.TabIndex = 1;
+            button2.Text = "Match Madness";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Showcard Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button3.Location = new Point(12, 118);
+            button3.Name = "button3";
+            button3.Size = new Size(236, 50);
+            button3.TabIndex = 2;
+            button3.Text = "Fill In The Blanks";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(475, 70);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 20);
+            label1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.Font = new Font("Showcard Gothic", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(414, 57);
+            label2.Name = "label2";
+            label2.Size = new Size(326, 289);
+            label2.TabIndex = 4;
+            label2.Text = "MATCH MADNESS IS A GAME WHERE THERE WILL BE 16 TILES AND PLAYER WILL HAVE TO MATCH THE CORRECT TILES BY THE READING THE WORDS ON THE TILES\"";
+            // 
             // HowToPlay
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
             ClientSize = new Size(982, 788);
             ControlBox = false;
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button3);
+            Controls.Add(button2);
             Controls.Add(button1);
             Name = "HowToPlay";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Howtoplay";
             Load += Howtoplay_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label1;
+        private Label label2;
     }
 }
--- Howtoplay.cs
+++ Howtoplay.cs
@@ -9,14 +9,15 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Son_of_Duo
-{ 
+{
     public partial class HowToPlay : Form
     {
-      
-       
+
+
         public HowToPlay()
         {
             InitializeComponent();
+            label2.Hide();
         }
 
         private void Howtoplay_Load(object sender, EventArgs e)
@@ -29,6 +30,20 @@ namespace Son_of_Duo
             this.Close();
             menu menu = new menu();
             menu.Show();
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Show();
+            label2.Text = "MATCH MADNESS IS A GAME WHERE THERE WILL BE 16 TILES AND PLAYER WILL HAVE TO MATCH THE CORRECT TILES BY THE READING THE WORDS ON THE TILES";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label2.Show();
+            label2.Text = "PLAYER WILL BE ASKED DIFFERENT QUESTION WITH ONE MISSING WORD IN BETWEEN AND PLAYER WILL HAVE TO ANSWER IT CORRECTLY IN ORDER TO WIN THE GAME";
         }
     }
 }
